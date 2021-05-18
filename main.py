69725aff65e634ef745bff27bc83cff48affc6fd
danaremar <danaremar@alum.us.es>
2021-05-18 21:01:47 +0200

    Allow to add documents into MongoDB
    Add all documents related with demand request.

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import json
 import datetime
 import csv
 from calendar import monthrange
+import pymongo
+
+START_YEAR = 2019
+END_YEAR = 2020
+
+my_client = pymongo.MongoClient("mongodb://localhost:27017/")
+my_db = my_client["ree-rest"]
+my_col = my_db["demand"]
 
 # DEMAND BY MONTH
 def demand_by_month(year,month):
@@ -19,6 +27,15 @@ def demand_by_years(start_year, end_year):
         for m in range(1,13):
             demand_by_month(y,m)
 
+# INSERT INTO DB
+def insert_into_db():
+    #demand_list = demand_by_years(START_YEAR, END_YEAR)
+    demand_list = demand_by_month(2020,2)
+    print(demand_list[0:2])
+    for i in demand_list:
+        date_transformed = i["datetime"][0:19]
+        my_dict = { "type": "demand", "value": i["value"], "datetime": datetime.datetime.strptime( date_transformed , "%Y-%m-%dT%H:%M:%S") }
+        my_col.insert_one(my_dict)
+
 # SCRIPT
-r = demand_by_month(2020,2)
-print(r[0])
+insert_into_db()
