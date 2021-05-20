d8c25fe9e7b051797d32ffccf82c2fac87247d25
danaremar <danaremar@alum.us.es>
2021-05-20 20:58:05 +0200

    Added some Fixes

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import requests
 import json
-import datetime
-import csv
+from datetime import datetime
 from calendar import monthrange
 import pymongo
 
 START_YEAR = 2019
 END_YEAR = 2020
 
+
 my_client = pymongo.MongoClient("mongodb://localhost:27017/")
 my_db = my_client["ree-rest"]
 my_col = my_db["demand"]
+
 
 # DEMAND BY MONTH
 def demand_by_month(year,month):
@@ -20,22 +21,42 @@ def demand_by_month(year,month):
     return requests.get(url).json()["included"][0]["attributes"]["values"]
 
 
-# DEMAND BY YEARS
-def demand_by_years(start_year, end_year):
+# INSERT BY MONTH INTO DB
+def insert_into_db_by_month(year, month):
+    demand_list = demand_by_month(year,month)
+
+    for i in demand_list:
+        date_string = i["datetime"][0:19]
+        date = datetime.strptime( date_string , "%Y-%m-%dT%H:%M:%S")
+        _id = datetime.timestamp(date)
+        my_dict = { "_id": _id, "type": "demand", "value": i["value"], "datetime": date }
+        my_col.insert_one(my_dict)
+
+    print( "insert_into_db_by_month() - Inserted for " + str(month) + "/" + str(year) )
+
+
+
+# INSERT INTO DB BY YEARS
+def insert_into_db_by_years(start_year, end_year):
     years = range(start_year, end_year + 1)
     for y in years:
         for m in range(1,13):
-            demand_by_month(y,m)
+            this_year = datetime.today().year
+            this_month = datetime.today().month
+            if this_year<=y and this_month<=m:
+                # not allowes calls to future, stop and don't continue
+                print("insert_into_db_by_years() - Future date")
+                return
+            insert_into_db_by_month(y, m)
 
-# INSERT INTO DB
-def insert_into_db():
-    #demand_list = demand_by_years(START_YEAR, END_YEAR)
-    demand_list = demand_by_month(2020,2)
-    print(demand_list[0:2])
-    for i in demand_list:
-        date_transformed = i["datetime"][0:19]
-        my_dict = { "type": "demand", "value": i["value"], "datetime": datetime.datetime.strptime( date_transformed , "%Y-%m-%dT%H:%M:%S") }
-        my_col.insert_one(my_dict)
 
-# SCRIPT
-insert_into_db()
+# DROP COLLECTION
+def drop():
+    my_col.drop()
+
+
+def run():
+    drop()
+    insert_into_db_by_years(START_YEAR, END_YEAR)
+
+run()
